02a3730be399817b4ba10d8a8d8697b4aea79f30
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2022-03-26 20:10:08 +0900

    feat: fadein animation

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,3 @@
-import * as React from 'react';
-import styles from './styles.module.css';
+import fadeIn from './fadeIn';
 
-interface Props {
-  text: string;
-}
-
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>;
-};
+export { fadeIn };
